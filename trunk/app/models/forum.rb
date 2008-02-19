9b7ece03e8765047b26e83027fa3b4f93b8deb0a
radarlistener <radarlistener@3e3f05cc-2437-0410-b010-b7b0f796eb59>
2008-02-19 08:08:25 +0000

    Commit this!

--- trunk/app/models/forum.rb
+++ trunk/app/models/forum.rb
@@ -4,6 +4,8 @@ class Forum < ActiveRecord::Base
   has_many :topics, :order => "sticky DESC, created_at DESC", :dependent => :destroy
   has_many :posts, :through => :topics, :source => :posts
   validates_presence_of :title, :description
+  belongs_to :visible_to, :class_name => "UserLevel", :foreign_key => "is_visible_to"
+  belongs_to :creator_of_topics, :class_name => "UserLevel", :foreign_key => "topics_created_by"
   
   def last_post
     topics.empty? ? nil : topics.last.posts.last
